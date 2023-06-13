26981b9619ee482bd04e6dfa8449d578c47e84b7
liang yutang <957753243@qq.com>
2023-06-13 09:42:04 +0800

    添加项目文件。

---
+++ Source/BomberMan/BomberManGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BomberManGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN_API ABomberManGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
