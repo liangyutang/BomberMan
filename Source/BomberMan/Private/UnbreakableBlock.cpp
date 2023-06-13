3e92786ca3324175c650cefda2a2a8984672e524
liang yutang <957753243@qq.com>
2023-06-13 11:38:58 +0800

    添加不可销毁障碍物及其生成类

--- Source/BomberMan/Private/UnbreakableBlock.cpp
+++ Source/BomberMan/Private/UnbreakableBlock.cpp
@@ -1,29 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BlockGenerator.h"
+#include "UnbreakableBlock.h"
 
 // Sets default values
-ABlockGenerator::ABlockGenerator()
+AUnbreakableBlock::AUnbreakableBlock()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	UnbreakableBlockSM = CreateDefaultSubobject < UStaticMeshComponent>("UnbreakableBlockSM");
+	UnbreakableBlockSM->SetupAttachment(RootComponent);
+
+
 }
 
 // Called when the game starts or when spawned
-void ABlockGenerator::BeginPlay()
+void AUnbreakableBlock::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-void ABlockGenerator::SpawnUnbreakableBlock()
-{
-}
-
 // Called every frame
-void ABlockGenerator::Tick(float DeltaTime)
+void AUnbreakableBlock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
--- Source/BomberMan/Public/BlockGenerator.h
+++ Source/BomberMan/Public/BlockGenerator.h
@@ -16,12 +16,23 @@ public:
 	ABlockGenerator();
 
 protected:
-	
 
+	UPROPERTY(EditAnywhere, Category = "Block Generation")
+	TSubclassOf <class  AUnbreakableBlock > UnbreakableBlock;
+
+	UPROPERTY(EditAnywhere, Category = "Block Generation | Setting")
+	int Length = 15;
+
+	UPROPERTY(EditAnywhere, Category = "Block Generation | Setting")
+	int Width = 15;
+
+	UPROPERTY(EditAnywhere, Category = "Block Generation | Setting")
+	float Scale = 100.0f;
 protected:
-	// Called when the game starts or when spawned
+	// Called when the game starts or when spawne
 	virtual void BeginPlay() override;
 
+	void SpawnUnbreakableBlock();
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
