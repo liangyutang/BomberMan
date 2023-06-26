9a0460fa558e0b76c988f2c63c0383d3ccc58460
liang yutang <957753243@qq.com>
2023-06-26 10:24:14 +0800

    道具类创建

---
+++ Source/BomberMan/Private/Powerup.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Powerup.h"
+
+#include "Components/BillboardComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+APowerup::APowerup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
+	BoxCollision->SetupAttachment(RootComponent);
+
+	Billboard = CreateDefaultSubobject<UBillboardComponent>("Billboard");
+	Billboard->SetupAttachment(BoxCollision);
+}
+
+// Called when the game starts or when spawned
+void APowerup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APowerup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BomberMan/Public/Powerup.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Powerup.generated.h"
+
+UCLASS()
+class BOMBERMAN_API APowerup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APowerup();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere,Category="Component")
+	class UBoxComponent* BoxCollision;
+
+	//´æ·ÅÌùÍ¼
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	class UBillboardComponent* Billboard;
+
+	//ÌùÍ¼Êý×é
+	UPROPERTY(VisibleAnywhere,Category="Component")
+	TArray<UTexture2D*> Sprite;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
