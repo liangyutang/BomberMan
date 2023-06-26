9ca141df96581fbf8a4f86f633d0c194396ccec9
liang yutang <957753243@qq.com>
2023-06-26 11:22:33 +0800

    创建游戏模式

--- Source/BomberMan/BomberManGameModeBase.cpp
+++ Source/BomberMan/BomberManGameModeBase.cpp
@@ -3,3 +3,11 @@
 
 #include "BomberManGameModeBase.h"
 
+#include "BomberCharacter.h"
+#include "BomberPlayerController.h"
+
+ABomberManGameModeBase::ABomberManGameModeBase()
+{
+	DefaultPawnClass = ABomberCharacter::StaticClass();
+	PlayerControllerClass = ABomberPlayerController::StaticClass();
+}
--- Source/BomberMan/BomberManGameModeBase.h
+++ Source/BomberMan/BomberManGameModeBase.h
@@ -13,5 +13,15 @@ UCLASS()
 class BOMBERMAN_API ABomberManGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ABomberManGameModeBase();
+protected:
+	//道具掉落几率
+
+	UPROPERTY(EditAnywhere,Category="Powerup", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	float DropChance = 0.4f;
+
+public:
+	FORCEINLINE float GetDropChance() const { return DropChance; }
 };
